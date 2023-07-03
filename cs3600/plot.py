b8a166132323b49a86b39c3d43bc1bb5449be72f
yotam_g <yotga1@gmail.com>
2023-07-03 19:09:44 +0300

    plot fix

--- cs3600/plot.py
+++ cs3600/plot.py
@@ -99,23 +99,18 @@ def plot_fit(fit_res: FitResult, fig=None, log_loss=False, legend=None):
     """
     if fig is None:
         fig, axes = plt.subplots(
-            nrows=2, ncols=2, figsize=(16, 10), sharex="col", sharey=False
+            nrows=2, ncols=2, figsize=(16, 10), sharex="col",
         )
         axes = axes.reshape(-1)
     else:
         axes = fig.axes
-
-    for ax in axes:
-        for line in ax.lines:
-            if line.get_label() == legend:
-                line.remove()
 
     p = itertools.product(["train", "test"], ["loss", "acc"])
     for idx, (traintest, lossacc) in enumerate(p):
         ax = axes[idx]
         attr = f"{traintest}_{lossacc}"
         data = getattr(fit_res, attr)
-        h = ax.plot(np.arange(1, len(data) + 1), data, label=legend)
+        ax.plot(np.arange(1, len(data) + 1), data, label=str(legend))
         ax.set_title(attr)
         if lossacc == "loss":
             ax.set_xlabel("Iteration #")
@@ -126,7 +121,7 @@ def plot_fit(fit_res: FitResult, fig=None, log_loss=False, legend=None):
         else:
             ax.set_xlabel("Epoch #")
             ax.set_ylabel("Accuracy (%)")
-        if legend:
+        if legend and (lossacc is not "loss"):
             ax.legend()
         ax.grid(True)
 
